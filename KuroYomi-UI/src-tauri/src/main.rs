46f5d5543f8cf3f149ce659253892ac90aba8dbf
Alejandro Palmar <alejandrojpalmar@hotmail.com>
2025-11-06 20:35:06 -0400

    Initial version that does nothing

--- KuroYomi-UI/src-tauri/src/main.rs
+++ KuroYomi-UI/src-tauri/src/main.rs
@@ -2,5 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    std::env::set_var("__GL_THREADED_OPTIMIZATIONS", "0");
+    std::env::set_var("__NV_DISABLE_EXPLICIT_SYNC", "1");
     kuroyomi_ui_lib::run()
 }
