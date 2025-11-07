0653fc4e2c056be8e50ce931ca3eea6166c07501
Alejandro Palmar <alejandrojpalmar@hotmail.com>
2025-11-07 09:38:24 -0400

    Started using Polars to normalize Yomitan/chan dictionaries

---
+++ KuroYomi-UI/pre-tauri-rust/target/debug/deps/pre_tauri_rust-e45c0cd114b05469.d
@@ -0,0 +1,5 @@
+/home/alejoseed/Projects/KuroYomi/KuroYomi-UI/pre-tauri-rust/target/debug/deps/libpre_tauri_rust-e45c0cd114b05469.rmeta: src/main.rs
+
+/home/alejoseed/Projects/KuroYomi/KuroYomi-UI/pre-tauri-rust/target/debug/deps/pre_tauri_rust-e45c0cd114b05469.d: src/main.rs
+
+src/main.rs:
