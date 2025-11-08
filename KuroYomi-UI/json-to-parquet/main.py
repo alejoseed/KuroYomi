c9c12c6f57fc24c80ea8a503e9ac9f8143fd2a3e
Alejandro Palmar <alejandrojpalmar@hotmail.com>
2025-11-08 10:08:48 -0400

    Added test reading json

--- KuroYomi-UI/json-to-parquet/main.py
+++ KuroYomi-UI/json-to-parquet/main.py
@@ -1,4 +1,7 @@
 import polars as pl
+import json
+import time
+from pathlib import Path
 
 term_bank_schema = {
     "column_0": pl.String,   # term
@@ -11,15 +14,60 @@ term_bank_schema = {
     "column_7": pl.String,   # term tags
 }
 
+def convert_json_to_dict(json_path : Path) -> list[list[str]]:
+    def get_json_as_str(json_path: Path):
+        with open(json_path) as f:
+            json_str = f.readline()
+            return json_str
 
+    json_str = get_json_as_str(json_path)
+
+    json_obj : list[list[str]] = json.loads(json_str)
+    
+    return json_obj
+
+def normalize_yomitan_json() -> str:
+    test = """
+        [
+            [
+                "マンシングウエア",
+                "マンシングウエア",
+                "n product",
+                "",
+                -200,
+                [
+                    {
+                        "content": {
+                            "content": {
+                                "content": "Munsingwear",
+                                "tag": "li"
+                            },
+                            "data": {
+                                "content": "glossary"
+                            },
+                            "lang": "en",
+                            "style": {
+                                "listStyleType": "circle"
+                            },
+                            "tag": "ul"
+                        },
+                        "type": "structured-content"
+                    }
+                ],
+                5741143,
+                ""
+            ]
+        ]
+    """
+
+    att = json.loads(test)
+
+    print(att)
+    return test
 def main():
-    test = pl.read_json(
-        "/home/alejoseed/Projects/KuroYomi/KuroYomi-UI/json-to-parquet/example.json",
-        schema=term_bank_schema,
-        infer_schema_length=None
-    )
-    print(test)
-
+    test_json_path = Path(f"/home/alejoseed/Projects/KuroYomi/KuroYomi-UI/json-to-parquet/JMDict/term_bank_52.json")
+    term_bank_52 = convert_json_to_dict(test_json_path)
+    print(term_bank_52)
 
 if __name__ == "__main__":
     main()
