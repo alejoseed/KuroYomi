0d3391a80dfbd414616b7c0c10fd67b297d39d21
Alejandro Palmar <alejandrojpalmar@hotmail.com>
2025-11-15 19:03:44 -0400

    Now I actually get the list the right way using a stack. I will probably insert to a database and search using full text search

--- KuroYomi-UI/pre-tauri-rust/src/main.rs
+++ KuroYomi-UI/pre-tauri-rust/src/main.rs
@@ -1,3 +1,20 @@
+// use polars::prelude::*;
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+    
+    loop {
+        let mut line = String::new();
+
+        match handle.read_line(&mut line) {
+            Ok(0) => break,
+            Ok(_) => print!("{}", line),
+            Err(e) => {
+                eprintln!("error {}", e);
+                break;
+            }
+        }
+    }
 }
