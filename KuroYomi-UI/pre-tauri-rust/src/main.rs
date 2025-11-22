6dab3e215da0f26806734b539e37fe4fe7341f02
Alejandro Palmar <alejandrojpalmar@hotmail.com>
2025-11-21 23:29:08 -0400

    Testing window creation using gpui

--- KuroYomi-UI/pre-tauri-rust/src/main.rs
+++ KuroYomi-UI/pre-tauri-rust/src/main.rs
@@ -1,6 +1,18 @@
 // use polars::prelude::*;
+use gpui::{
+    App, Application, Bounds, Context, SharedString, Window, WindowBounds, WindowOptions, div,
+    prelude::*, px, rgb, rgba, size, white,
+};
 use rusqlite::{Connection, Result};
 use std::io::{self, BufRead};
+
+struct HelloWorld {
+    text: SharedString,
+}
+
+struct OverlayApp {
+    selected_text: SharedString,
+}
 
 #[derive(Debug)]
 struct Definitions {
@@ -60,20 +72,94 @@ fn find_in_dict(word: &str) -> Result<()> {
     Ok(())
 }
 
-fn main() {
-    let stdin = io::stdin();
-    let mut handle = stdin.lock();
-
-    loop {
-        let mut line = String::new();
-
-        match handle.read_line(&mut line) {
-            Ok(0) => break,
-            Ok(_) => find_in_dict(&line.trim_end()).expect("Something crazy happened"),
-            Err(e) => {
-                eprintln!("error {}", e);
-                break;
-            }
-        }
+impl Render for OverlayApp {
+    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
+        div()
+            .size_full()
+            .bg(white())
+            .child(div().child(format!("Selected {}", self.selected_text)))
     }
 }
+
+impl Render for HelloWorld {
+    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
+        div()
+            .flex()
+            .flex_col()
+            .gap_3()
+            .bg(rgb(0x505050))
+            .size(px(500.0))
+            .justify_center()
+            .items_center()
+            .shadow_lg()
+            .border_1()
+            .border_color(rgb(0x0000ff))
+            .text_xl()
+            .text_color(rgb(0xffffff))
+            .child(format!("Hello, {}!", &self.text))
+            .child(
+                div()
+                    .flex()
+                    .gap_2()
+                    .child(div().size_8().bg(gpui::red()))
+                    .child(div().size_8().bg(gpui::green()))
+                    .child(div().size_8().bg(gpui::blue()))
+                    .child(div().size_8().bg(gpui::yellow()))
+                    .child(div().size_8().bg(gpui::black()))
+                    .child(div().size_8().bg(gpui::white())),
+            )
+    }
+}
+
+fn main() {
+    Application::new().run(|cx: &mut App| {
+        let bounds = Bounds::centered(None, size(px(500.), px(500.0)), cx);
+
+        cx.open_window(
+            WindowOptions {
+                titlebar: None,
+                window_bounds: Some(WindowBounds::Windowed(bounds)),
+                ..Default::default()
+            },
+            |_, cx| {
+                cx.new(|_| OverlayApp {
+                    selected_text: "No text selected".into(),
+                })
+            },
+        )
+        .unwrap();
+    });
+}
+// fn main() {
+//     Application::new().run(|cx: &mut App| {
+//         let bounds = Bounds::centered(None, size(px(500.), px(500.0)), cx);
+//         cx.open_window(
+//             WindowOptions {
+//                 window_bounds: Some(WindowBounds::Windowed(bounds)),
+//                 ..Default::default()
+//             },
+//             |_, cx| {
+//                 cx.new(|_| HelloWorld {
+//                     text: "World".into(),
+//                 })
+//             },
+//         )
+//         .unwrap();
+//     });
+// }
+
+// let stdin = io::stdin();
+// let mut handle = stdin.lock();
+
+// loop {
+// let mut line = String::new();
+
+//     match handle.read_line(&mut line) {
+//         Ok(0) => break,
+//         Ok(_) => find_in_dict(&line.trim_end()).expect("Something crazy happened"),
+//         Err(e) => {
+//             eprintln!("error {}", e);
+//             break;
+//         }
+//     }
+// }
